1cddf459c9e5d244ea1b5b3aadd4c2147c0ef91b
Emanuel Neves <emanuel.neves@protonmail.com>
2019-03-11 23:12:58 -0400

    new component for todos

--- src/App.js
+++ src/App.js
@@ -3,10 +3,29 @@ import Todos from './components/Todos';
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        title: "Take out the trash",
+        id : 1,
+        completed: false
+      },
+      {
+        title: "Buy groceries",
+        id : 2,
+        completed: false
+      },
+      {
+        title: "Clean up after Charlie",
+        id : 3,
+        completed: false
+      }
+    ]
+  }
   render() {
     return (
       <div className="App">
-        <Todos/>
+        <Todos todos={this.state.todos}/>
       </div>
     );
   }
